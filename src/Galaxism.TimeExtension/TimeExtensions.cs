9b035a745614602329a5656ff19972394cf8923e
rabbitism <popmessiah@126.com>
2020-04-02 23:15:45 +0800

    Initialize Project

---
+++ src/Galaxism.TimeExtension/TimeExtensions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Galaxism.TimeExtension
+{
+    public static class TimeExtensions
+    {
+        /// <summary>
+        /// Returns a new System.DateTime that adds the specified number of days to the value of this instance. This method is in ticks precision instead of millisecond precision.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="days">A number of whole and fractional days. The value parameter can be negative or positive.</param>
+        /// <returns>An object whose value is the sum of the date and time represeted by this instance and the number of days represented by value</returns>
+        public static DateTime AddPreciseDays(this DateTime dateTime, double days)
+        {
+            return dateTime.AddTicks((long)(days * TimeSpan.TicksPerDay));
+        }
+
+        /// <summary>
+        /// Returns a new System.DateTime that adds the specified number of hours to the value of this instance. This method is in ticks precision instead of millisecond precision.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="hours">A number of whole and fractional hours. The value parameter can be negative or positive.</param>
+        /// <returns></returns>
+        public static DateTime AddPreciseHours(this DateTime dateTime, double hours)
+        {
+            return dateTime.AddTicks((long)(hours * TimeSpan.TicksPerHour));
+        }
+
+        /// <summary>
+        /// Returns a new System.DateTime that adds the specified number of minutes to the value of this instance. This method is in ticks precision instead of millisecond precision.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="minutes">A number of whole and fractional minutes. The value parameter can be negative or positive.</param>
+        /// <returns></returns>
+        public static DateTime AddPreciseMinutes(this DateTime dateTime, double minutes)
+        {
+            return dateTime.AddTicks((long)(minutes * TimeSpan.TicksPerMinute));
+        }
+
+        /// <summary>
+        /// Returns a new System.DateTime that adds the specified number of seconds to the value of this instance. This method is in ticks precision instead of millisecond precision.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="seconds">A number of whole and fractional seconds. The value parameter can be negative or positive.</param>
+        /// <returns></returns>
+        public static DateTime AddPreciseSeconds(this DateTime dateTime, double seconds)
+        {
+            return dateTime.AddTicks((long)(seconds * TimeSpan.TicksPerSecond));
+        }
+
+        /// <summary>
+        /// Returns a new System.DateTime that adds the specified number of milliseconds to the value of this instance. This method is in ticks precision instead of millisecond precision.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="milliseconds">>A number of whole and fractional milliseconds. The value parameter can be negative or positive.</param>
+        /// <returns></returns>
+        public static DateTime AddPreciseMilliseconds(this DateTime dateTime, double milliseconds)
+        {
+            return dateTime.AddTicks((long)(milliseconds * TimeSpan.TicksPerMillisecond));
+        }
+
+        /// <summary>
+        /// Returns a new System.DateTime that adds the specified number of milliseconds to the value of this instance. This method is in ticks precision instead of millisecond precision.
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="microseconds">A number of whole and fractional microseconds. The value parameter can be negative or positive.</param>
+        /// <returns></returns>
+        public static DateTime AddMicroseconds(this DateTime dateTime, double microseconds)
+        {
+            return dateTime.AddTicks((long)(microseconds * 10));
+        }
+
+        public static int GetMicroseconds(this DateTime dateTime)
+        {
+            return (int)(dateTime.Ticks % 10000) / 10;
+        }
+
+        public static int Microseconds(this TimeSpan span)
+        {
+            return (int)(span.Ticks % 10000) / 10;
+        }
+
+        public static double TotalMicroseconds(this TimeSpan span)
+        {
+            return span.Ticks / 10.0;
+        }
+    }
+}
---
+++ src/Galaxism.TimeExtensionTests/TimeExtensionsTests.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Galaxism.TimeExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxism.TimeExtension.Tests
+{
+    [TestClass()]
+    public class TimeExtensionsTests
+    {
+        public static IEnumerable<object[]> OfferData()
+        {
+            Random random = new Random();
+            DateTime baseTime = new DateTime(2020, 4, 2);
+            for (int i = 0; i < 10; i++)
+            {
+                yield return new object[] { baseTime, random.NextDouble() };
+                yield return new object[] { baseTime, -random.NextDouble() };
+            }
+        }
+
+        [TestMethod()]
+        [DynamicData(nameof(OfferData), DynamicDataSourceType.Method)]
+        public void AddPreciseDaysTest(DateTime date, double day)
+        {
+            DateTime date1 = date.AddPreciseDays(day);
+            DateTime date2 = date.AddDays(day);
+            Assert.AreNotEqual(0, date1.Ticks % 10000);
+            Assert.AreEqual(0, date2.Ticks % 10000);
+            Assert.IsTrue(Math.Abs(date1.Ticks / 10000 - date2.Ticks / 10000) <= 1);
+        }
+
+        [TestMethod()]
+        [DynamicData(nameof(OfferData), DynamicDataSourceType.Method)]
+        public void AddPreciseHoursTest(DateTime date, double hours)
+        {
+            DateTime date1 = date.AddPreciseHours(hours);
+            DateTime date2 = date.AddHours(hours);
+            Assert.AreNotEqual(0, date1.Ticks % 10000);
+            Assert.AreEqual(0, date2.Ticks % 10000);
+            Assert.IsTrue(Math.Abs(date1.Ticks / 10000 - date2.Ticks / 10000) <= 1);
+        }
+
+        [TestMethod()]
+        [DynamicData(nameof(OfferData), DynamicDataSourceType.Method)]
+        public void AddPreciseMinutesTest(DateTime date, double minutes)
+        {
+            DateTime date1 = date.AddPreciseMinutes(minutes);
+            DateTime date2 = date.AddMinutes(minutes);
+            Assert.AreNotEqual(0, date1.Ticks % 10000);
+            Assert.AreEqual(0, date2.Ticks % 10000);
+            Assert.IsTrue(Math.Abs(date1.Ticks / 10000 - date2.Ticks / 10000) <= 1);
+        }
+
+        [TestMethod()]
+        [DynamicData(nameof(OfferData), DynamicDataSourceType.Method)]
+        public void AddPreciseSecondsTest(DateTime date, double seconds)
+        {
+            DateTime date1 = date.AddPreciseSeconds(seconds);
+            DateTime date2 = date.AddSeconds(seconds);
+            Assert.AreNotEqual(0, date1.Ticks % 10000);
+            Assert.AreEqual(0, date2.Ticks % 10000);
+            Assert.IsTrue(Math.Abs(date1.Ticks / 10000 - date2.Ticks / 10000) <= 1);
+        }
+
+        [TestMethod()]
+        [DynamicData(nameof(OfferData), DynamicDataSourceType.Method)]
+        public void AddPreciseMillisecondsTest(DateTime date, double milliseconds)
+        {
+            DateTime date1 = date.AddPreciseMilliseconds(milliseconds);
+            DateTime date2 = date.AddMilliseconds(milliseconds);
+            Assert.AreNotEqual(0, date1.Ticks % 10000);
+            Assert.AreEqual(0, date2.Ticks % 10000);
+            Assert.IsTrue(Math.Abs(date1.Ticks / 10000 - date2.Ticks / 10000) <= 1);
+        }
+
+        public static IEnumerable<object[]> AddMicrosecondsTestData()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                yield return new object[] { DateTime.Now, 100000000 * random.NextDouble() };
+            }
+        }
+
+        [TestMethod()]
+        [DynamicData(nameof(AddMicrosecondsTestData), DynamicDataSourceType.Method)]
+        public void AddMicrosecondsTest(DateTime dateTime, double diff)
+        {
+            DateTime date = dateTime.AddMicroseconds(diff);
+            Assert.AreEqual((long)(diff * 10), date.Ticks - dateTime.Ticks);
+        }
+
+        [TestMethod()]
+        public void GetMicrosecondsTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void MicrosecondsTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void TotalMicrosecondsTest()
+        {
+            
+        }
+    }
+}
